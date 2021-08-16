e59ea44cd41fbfeb584269a4d8bac11dc48893b4
Soham Ghosal <48046283+soham1192k@users.noreply.github.com>
2021-08-16 16:37:21 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -3,40 +3,44 @@
 using namespace std;
 /********************* Custom Inputs ***************************/
 int n_low=1;
-int n_high=10;
-int s_low=1;
-int s_high=2500;
+int n_high=1e5;
+long long cars_low=1;
+long long cars_high=1e14;
+int k_high=1e5;
 /********************* Custom Inputs ***************************/
 #define endl '\n'
 void generate();
 int main(int argc, char* argv[]){
-    registerGen(argc, argv, 1);
-    string type = argc > 1 ? argv[1] : "unexpected";
+	registerGen(argc, argv, 1);
+	string type = argc > 1 ? argv[1] : "unexpected";
+	
+	if(type == "small"){
+		n_high = 10;
+        cars_high=20;
+	}
 
-    if(type == "small"){
-      n_high = 1;
-          s_high = 10;
+    if(type == "custom"){
+        n_low = n_high;
+        cars_low = 1e10;
+        k_high = 5;
     }
 
-    if(type == "extreme"){
-      n_low = n_high;
-      s_low = s_high;
-    }
+	if(type == "extreme"){
+		n_low = n_high;
+	}
 
-    generate();
-    return 0;
+	generate();
+	return 0;
 }
 void generate(){
-    int n = rnd.next(n_low, n_high);
-    cout << n << endl;
-    vector<char>hold = {'L','R','G'};
-    for(int i = 1; i <= n; i++){
-      int len = rnd.next(s_low,s_high);
-      string now = "";
-      for(int j = 1; j <= len; j++){
-          int val = rnd.next(1,3);
-          now += hold[val-1];
-      }
-      cout << now << endl;
+	int n = rnd.next(n_low, n_high);
+	cout << n << endl;
+    set<long long>s;
+    while((int)s.size() != n){
+        s.insert(rnd.next(cars_low,cars_high));
     }
+    for(auto &ele : s) 
+    cout << ele << endl;
+    int k = rnd.next(1,min(k_high,n));
+    cout << k << endl;
 }
